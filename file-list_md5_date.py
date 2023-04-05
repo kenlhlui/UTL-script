681ad5134a4b29ac999e6b39972b74ba4899880a
Ken Lui <kenlh.lui@mail.utoronto.ca>
2023-04-05 10:54:11 -0400

    Added csvname phraser 1. Added -c csvname phraser to let user name the csv file 2. Fixed bug on the logdirectory path by adding "/"

--- file-list_md5_date.py
+++ file-list_md5_date.py
@@ -2,12 +2,52 @@ import os
 import csv
 import hashlib
 import time
+import argparse
 
-### Specify the directory you want to list files for ###
-directory = "/mnt/data/DISK-1509"
+#######################
+###### ARGUMENTS ######
+#######################
 
-# Specify the extensions of files you want
-targed_ext = ('.json','.tiff','.iso')
+parser = argparse.ArgumentParser(
+	description ="Script to check the files with same name and different file tpye, Mar 2023")
+
+parser.add_argument(
+        '-d','--dir', type=str,
+        help='Targed directory, e.g. /home/test/targeted_directory', required=True)
+parser.add_argument(
+        '-l','--logdir', type=str,
+        help='Projectlog output directory, e.g. /home/test/output/', required=False)
+
+parser.add_argument(
+        '-c','--csvname', type=str,
+        help='Projectlog csv name, e.g. 1509.csv ', required=False)
+
+parser.add_argument(
+        '-e','--ext', nargs="+", type=str,
+        help='Targeted extensions, e.g. .iso .tiff', required=False)
+
+## Array for all args passed to script
+args = parser.parse_args()
+
+###############################
+########## VARIABLES ##########
+###############################
+
+directory = args.dir
+
+if args.logdir:
+	logdirectory = args.logdir
+else:
+	logdirectory = ""
+
+if args.csvname:
+	csvname = args.csvname
+else:
+	csvname = "file-list.csv"
+
+
+targed_ext = tuple(args.ext)
+
 
 # Create a dictionary to store the file types and their respective file names
 file_dict = {}
@@ -24,7 +64,7 @@ for file in [f for f in os.listdir(directory) if f.endswith(targed_ext)]:
         file_dict[ext] = [name]
 
 # Write the file names and types to a CSV file, sorted by file type
-with open("file_list.csv", "w", newline="") as file:
+with open(str(logdirectory) + "/" + str(csvname), "w", newline="") as file:
     writer = csv.writer(file)
     # Write the headers for the columns
     headers = ["File_name"]
@@ -47,16 +87,19 @@ with open("file_list.csv", "w", newline="") as file:
         # For iso files md5 checksum & modification time
         if name in file_dict[ext]:
             iso_file_path = str(directory) + "/" + str(name) + ".iso" # Define iso file path
-            modification_time = str(time.ctime(os.path.getmtime(iso_file_path))) # Get modification time of iso file
-            with open(iso_file_path, 'rb') as file:
-                md5_hash = hashlib.md5()
-                chunk = 0
-                while chunk != b'':
-                    chunk = file.read(1024)
-                    md5_hash.update(chunk)
-            md5_checksum = md5_hash.hexdigest()
-            row.append(md5_checksum) # Add the iso file checksum to the row
-            row.append(modification_time) # Add the iso file modfication time to the row
+            try:
+                modification_time = str(time.ctime(os.path.getmtime(iso_file_path))) # Get modification time of iso file
+                with open(iso_file_path, 'rb') as file:
+                    md5_hash = hashlib.md5()
+                    chunk = 0
+                    while chunk != b'':
+                        chunk = file.read(1024)
+                        md5_hash.update(chunk)
+                md5_checksum = md5_hash.hexdigest()
+                row.append(md5_checksum) # Add the iso file checksum to the row
+                row.append(modification_time) # Add the iso file modfication time to the row
+            except:
+                pass
         else:
             row.append("")
         writer.writerow(row)
