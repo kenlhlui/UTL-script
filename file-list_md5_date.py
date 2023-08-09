a2534e1e8254446b4d0bdc28c953f20d4b2aa91a
Ken Lui <kenlh.lui@mail.utoronto.ca>
2023-08-09 13:27:21 -0400

    push
    push

--- file-list_md5_date.py
+++ file-list_md5_date.py
@@ -2,13 +2,18 @@ import os
 import csv
 import hashlib
 import time
+<<<<<<< Updated upstream
 import argparse
+=======
+import argparse 
+>>>>>>> Stashed changes
 
 #######################
 ###### ARGUMENTS ######
 #######################
 
 parser = argparse.ArgumentParser(
+<<<<<<< Updated upstream
 	description ="Script to check the files with same name and different file tpye, Mar 2023")
 
 parser.add_argument(
@@ -25,6 +30,15 @@ parser.add_argument(
 parser.add_argument(
         '-e','--ext', nargs="+", type=str,
         help='Targeted extensions, e.g. .iso .tiff', required=False)
+=======
+	description ="Script to list out specific file tpyes with same names to csv")
+parser.add_argument(
+        '-d','--dir', type=str,
+        help='Targeted directory', required=True)
+parser.add_argument(
+        '-e', '--ext', type=str,  nargs='+',
+        help='Targeted file extension(s)')
+>>>>>>> Stashed changes
 
 ## Array for all args passed to script
 args = parser.parse_args()
@@ -33,6 +47,7 @@ args = parser.parse_args()
 ########## VARIABLES ##########
 ###############################
 
+<<<<<<< Updated upstream
 directory = args.dir
 
 if args.logdir:
@@ -48,12 +63,19 @@ else:
 
 targed_ext = tuple(args.ext)
 
+=======
+dir = args.dir
+targed_ext = tuple(args.ext)
+
+# Specify the extensions of files you want
+#targed_ext = ('.json','.tiff','.iso')
+>>>>>>> Stashed changes
 
 # Create a dictionary to store the file types and their respective file names
 file_dict = {}
 
 # Loop through the targed files in the directory and group them by file type
-for file in [f for f in os.listdir(directory) if f.endswith(targed_ext)]:
+for file in [f for f in os.listdir(dir) if f.endswith(targed_ext)]:
     # Do something with the file:
     # Split the file name and extension
     name, ext = os.path.splitext(file)
@@ -64,15 +86,21 @@ for file in [f for f in os.listdir(directory) if f.endswith(targed_ext)]:
         file_dict[ext] = [name]
 
 # Write the file names and types to a CSV file, sorted by file type
+<<<<<<< Updated upstream
 with open(str(logdirectory) + "/" + str(csvname), "w", newline="") as file:
+=======
+csv_name = 'file_list_' + str(os.path.basename(dir)) + '.csv'
+with open(csv_name, "w", newline="") as file:
+>>>>>>> Stashed changes
     writer = csv.writer(file)
     # Write the headers for the columns
     headers = ["File_name"]
     for ext in sorted(file_dict.keys()):
         if ext in targed_ext:
             headers.append(ext)
-    headers.append("iso_md5-checksum")
-    headers.append("iso_modification_time")
+    if "iso" in file_dict[ext]:
+        headers.append("iso_md5-checksum")
+        headers.append("iso_modification_time")
     writer.writerow(headers)
     # Write each file type, iso file md5 checksum and modification time to rows
     for name in sorted(set([name for names in file_dict.values() for name in names])):
@@ -84,6 +112,7 @@ with open(str(logdirectory) + "/" + str(csvname), "w", newline="") as file:
                     row.append(name+ext)
                 else:
                     row.append("")
+<<<<<<< Updated upstream
         # For iso files md5 checksum & modification time
         if name in file_dict[ext]:
             iso_file_path = str(directory) + "/" + str(name) + ".iso" # Define iso file path
@@ -100,6 +129,21 @@ with open(str(logdirectory) + "/" + str(csvname), "w", newline="") as file:
                 row.append(modification_time) # Add the iso file modfication time to the row
             except:
                 pass
+=======
+        # For iso files md5 checksum & modification time ##BROKEN####
+        if ".iso" in file_dict[ext]:
+            iso_file_path = str(dir) + "/" + str(name) + ".iso" # Define iso file path
+            modification_time = str(time.ctime(os.path.getmtime(iso_file_path))) # Get modification time of iso file
+            with open(iso_file_path, 'rb') as file:
+                md5_hash = hashlib.md5()
+                chunk = 0
+                while chunk != b'':
+                    chunk = file.read(1024)
+                    md5_hash.update(chunk)
+            md5_checksum = md5_hash.hexdigest()
+            row.append(md5_checksum) # Add the iso file checksum to the row
+            row.append(modification_time) # Add the iso file modfication time to the row
+>>>>>>> Stashed changes
         else:
             row.append("")
         writer.writerow(row)
